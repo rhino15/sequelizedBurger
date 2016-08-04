a7fdf8ee86bafb54f6866669ae7481f32c3596eb
Roy Hinojosa <roy.hinojosa@gmail.com>
2016-08-04 01:18:10 -0500

    made changes to seeders, not sure if that's what i need to for homework

---
+++ seeders/20160804054737-BurgersTableSeeder.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Burgers', [{
+        burger_name: 'Shake Your Honeymaker Burger (comes with honey mustard)',
+        devoured: false
+      }, {
+        burger_name: "I've Created a Muenster Burger",
+        devoured: false
+      }, {
+        burger_name: 'If Looks Could Kale Burger',
+        devoured: false
+      }], {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('Burgers', null, {});
+  }
+};
